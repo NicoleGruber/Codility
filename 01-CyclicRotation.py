ddd7c13430f811e736d9952fd108ac4d9c059634
NicoleGruber <Nica.gruber@gmail.com>
2020-02-28 10:27:58 -0300

    CyclicRotation

---
+++ 01-CyclicRotation.py
@@ -0,0 +1,41 @@
+'''
+Uma matriz A consistindo de N inteiros é fornecida. A rotação da matriz significa que cada
+elemento é deslocado para a direita por um índice e o último elemento da matriz é movido para
+o primeiro lugar. Por exemplo, a rotação da matriz A = [3, 8, 9, 7, 6] é [6, 3, 8, 9, 7]
+(os elementos são deslocados para a direita por um índice e 6 é movido para o primeiro lugar).
+O objetivo é girar a matriz A K vezes; isto é, cada elemento de A será deslocado para o K tempo
+certo.
+Escreva uma função:
+solução def (A, K)
+que, dada uma matriz A que consiste em N números inteiros e um número inteiro K, retorna a
+A girada K vezes.
+Por exemplo, dado
+    A = [3, 8, 9, 7, 6]
+    K = 3
+a função deve retornar [9, 7, 6, 3, 8]. Foram realizadas três rotações:
+    [3, 8, 9, 7, 6] -> [6, 3, 8, 9, 7]
+    [6, 3, 8, 9, 7] -> [7, 6, 3, 8, 9]
+    [7, 6, 3, 8, 9] -> [9, 7, 6, 3, 8]
+Por outro exemplo, dado
+    A = [0, 0, 0]
+    K = 1
+a função deve retornar [0, 0, 0]
+Dado
+    A = [1, 2, 3, 4]
+    K = 4
+a função deve retornar [1, 2, 3, 4]
+Assuma isso:
+N e K são números inteiros dentro do intervalo [0..100];
+cada elemento da matriz A é um número inteiro dentro do intervalo [-1.000..1.000].
+Na sua solução, concentre-se na correção. O desempenho da sua solução não será o foco da avaliação.
+'''
+
+A = [1, 2, 3, 4]
+
+def solution(A,K):
+    if not A:
+        return A
+    for vezes in range(K):
+        ultimo_valor = A.pop() #A Função pop pega o ultimo valor da lista, nesse caso esse valor foi salvo em uma variável
+        A.insert(0, ultimo_valor) #Inserindo na lista no indice 0 o valor que que foi salvo na variável
+    return A
