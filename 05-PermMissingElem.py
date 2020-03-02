e9e95aeb035baa7bdbb2b2cba03f49c249579935
NicoleGruber <57449553+NicoleGruber@users.noreply.github.com>
2020-03-01 22:51:17 -0300

    PermMissingElem

--- 05-PermMissingElem.py
+++ 05-PermMissingElem.py
@@ -25,21 +25,14 @@ cada elemento da matriz A é um número inteiro dentro do intervalo [1 .. (N '''
 A = [2]
 
 def solution(A):
-    A.sort()
-    if len(A) == 0 or not 1 in A:
+    A.sort()                        #Colocando a lista em ordem crescente
+    if len(A) == 0 or not 1 in A:   #Verificando se os itens da lista  são == 0 ou se não tem o 1 na lista
         return 1
-    if len(A) == 1:
+    if len(A) == 1:                 #Verificando se os itens da lista são == 1
         return A[0]+1
-    cont = 1
-    for num in A:
-        if num != cont:
-            return cont
+    cont = 1                        #Contador para verificação dos numeros inteiros da lista
+    for num in A:                   #For para passar por todos os itens da lista
+        if num != cont:             #Verificando se o numero é diferente do contador
+            return cont             #Se for, ele retorna esse numero do contado que não tem na lista
         cont += 1
-    return cont
-
-
-def SS(A):
-    if len(A) == 1:
-        return A[0] + 1
-print(solution(A))
-
+    return cont                     #Se o numero que tiver faltando for o depois, não o que esta no meio, ele retorna esse numero
